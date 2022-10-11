f9fb33a9f8a593239eed7c9dba9dbb1175b93411
HoangAnhVIE <hoanganhnavn00@gmail.com>
2022-10-11 20:19:59 +0700

    edit

--- themoi.js
+++ themoi.js
@@ -3,14 +3,4 @@ var b=15;
 var c = a+b;
 console.log(`c do dap an la ${c} `);
 
-neu 1 ngay em khoc uot doi bo mi
-thin pro khong yeu 1 ai 
-ocs asdasdasd
 
-
-777777777777777777777777777777777777777777777777777799999999999999999999999999999999999999999
-
-
-
-165465465
-thin nggu
